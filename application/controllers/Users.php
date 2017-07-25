1003f278e42159a670e5d0a113ac6ef67c998040
Maco Cortes <maco.techdepot@gmail.com>
2017-07-25 21:26:12 +0800

    fixed redirect

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -78,7 +78,7 @@ class Users extends CI_Controller {
 		} else {
 
 			$this->session->set_flashdata('error', 'You need to login!');
-			redirect('sys/dashboard/login', 'refresh');
+			redirect('dashboard/login', 'refresh');
 		}
 
 	}

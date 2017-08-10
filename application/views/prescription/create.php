5850eefaaa8f5663ebf2551af11a146a207e3534
Maco Cortes <maco.techdepot@gmail.com>
2017-08-10 22:18:25 +0800

    Prescriptions View

---
+++ application/views/prescription/create.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="msapplication-tap-highlight" content="no">
+    <title><?=$title?> &middot; <?=$site_title?></title>
+
+    <?php $this->load->view('inc/css'); ?>
+
+
+</head>
+
+<body>
+    
+    <?php $this->load->view('inc/header'); ?>
+
+    <!-- //////////////////////////////////////////////////////////////////////////// -->
+
+
+  <!-- START MAIN -->
+  <div id="main">
+    <!-- START WRAPPER -->
+    <div class="wrapper">
+
+      <?php $this->load->view('inc/left_nav'); ?>
+
+      <!-- //////////////////////////////////////////////////////////////////////////// -->
+
+      <!-- START CONTENT -->
+      <section id="content">
+        
+        <!--breadcrumbs start-->
+        <div id="breadcrumbs-wrapper" class=" grey lighten-3">
+          <div class="container">
+            <div class="row">
+              <div class="col s12 m12 l12">
+                <h5 class="breadcrumbs-title"><?=$title?></h5>
+                <ol class="breadcrumb">
+                    <li><a href="<?=base_url()?>">Dashboard</a></li>
+                    <li><a href="<?=base_url('patients')?>">Patients</a></li>
+                    <li><a href="<?=base_url('patients/view/'.$info['id'])?>"><?=$info['fullname'] . ' ' . $info['lastname']?></a></li>
+                    <li><a href="<?=base_url('patients/view/'.$info['id'].'/case/'.$case['id'])?>"><?=$case['title']?></a></li>
+                    <li class="active"><?=$title?></li>
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--breadcrumbs end-->
+        
+
+        <!--start container-->
+        <div class="container">
+          <div class="section">
+            <div class="row">
+              <div class="col s12">
+                <?php
+                //ERROR ACTION                          
+                  if($this->session->flashdata('error')) { ?>
+                    <div class="card-panel deep-orange darken-3">
+                        <span class="white-text"><i class="mdi-alert-warning tiny"></i> <?php echo $this->session->flashdata('error'); ?></span>
+                    </div>
+              <?php } ?> 
+              <?php
+                //SUCCESS ACTION                          
+                  if($this->session->flashdata('success')) { ?>
+                    <div class="card-panel green">
+                        <span class="white-text"><i class="mdi-action-done tiny"></i> <?php echo $this->session->flashdata('success'); ?></span>
+                    </div>
+              <?php } ?>             
+              <?php
+                //FORM VALIDATION ERROR
+                    $this->form_validation->set_error_delimiters('<p><i class="mdi-alert-warning tiny"></i> ', '</p>');
+                      if(validation_errors()) { ?>
+                    <div class="card-panel yellow amber">
+                        <span class="white-text"> <?php echo validation_errors(); ?></span>
+                    </div>
+              <?php } ?> 
+              </div>
+            </div>
+            
+            <div class="row">
+              <div class="col s12">
+                <div class="card">
+                  <div class="card-content">
+                      <div class="row">
+                        <div class="input-field col s10">
+                          <input type="text" name="" id="" class="validate" />
+                          <label for="">Item</label>
+                        </div><!-- /.input-field -->
+                        <div class="input-field col s2">
+                          <button type="submit" class="btn waves-effect green">Add item</button>
+                        </div><!-- /.input-field -->
+                      </div><!-- /.row -->
+                      <div class="divider"></div><!-- /.divider -->
+                      <div class="row">
+                        <div class="col s12">
+                          <table class="bordered striped">
+                            <thead>
+                              <tr>
+                                <th>Item Name</th>
+                                <th width="5%">Qty</th>
+                                <th>Remarks</th>
+                                <th></th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              <tr>
+                                <td>Maco</td>
+                                <td>10</td>
+                                <td>10 times a week</td>
+                                <td></td>
+                              </tr>
+                            </tbody>
+                          </table><!-- /.bordered striped -->
+                        </div><!-- /.col s12 -->
+                      </div><!-- /.row -->
+                  </div><!-- /.card-content -->
+                </div><!-- /.card -->
+              </div><!-- /.col s12 -->
+            </div><!-- /.row -->
+         
+          </div>
+        </div>
+        <!--end container-->
+      </section>
+      <!-- END CONTENT -->
+
+    </div>
+    <!-- END WRAPPER -->
+
+  </div>
+  <!-- END MAIN -->
+
+
+
+     <!-- //////////////////////////////////////////////////////////////////////////// -->
+
+    <?php $this->load->view('inc/footer'); ?>
+
+    <?php $this->load->view('inc/js'); ?>
+   
+</body>
+</html>

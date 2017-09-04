a2cf6559696a4df0bc0570ade0c3868e15427cbf
Maco Cortes <maco.techdepot@gmail.com>
2017-09-04 20:20:49 +0800

    added debugging logs

--- application/config/config.php
+++ application/config/config.php
@@ -229,7 +229,7 @@ $config['allow_get_array'] = TRUE;
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 0;
+$config['log_threshold'] = 1;
 
 /*
 |--------------------------------------------------------------------------
